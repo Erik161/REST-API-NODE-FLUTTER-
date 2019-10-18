fd2960c354b6fd0c9ce2e373dd94255b92079977
erikhernandez <erikestuardo_161@hotmail.com>
2019-10-18 01:14:15 -0600

    add pubspec.yaml

--- usersapp/lib/main.dart
+++ usersapp/lib/main.dart
@@ -1,4 +1,10 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; //It is to have the material design
+import 'package:http/http.dart' as http; //is to make HTTP requests
+import 'dart:async'; /*is to handle asynchronous code 
+it will take some time to do the query to the server and get the data
+*/
+import 'dart:convert'; /*to convert the data that we will receive from the backend in JSON
+ format and display it in our interface*/
 
 void main() {
   runApp(
@@ -14,9 +20,17 @@ class HomePage extends StatefulWidget{
 }
 
 class _HomePageState extends State<HomePage> {
+  getUsers() async{
+  http.Response response = await http.get('http://localhost:4000/api/users');
+  debugPrint(response.body);
+  }
+
   Widget build(BuildContext context){
     return Scaffold(
-      appBar: AppBar(title: Text('User List ') ),
+      appBar: AppBar(title: Text('User List'),
+      backgroundColor: Colors.indigo[900],
+       ),
+      body: ListView.builder(), 
       );
   }
 }
